65626b1cd62c8d01972bab221249dadbb5c5a60b
klyltongomes <klyltongomesdesouza@MacBook-Pro-de-klylton.local>
2025-03-23 13:17:31 -0300

    Config Routes e criado components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app/app.component'; // Ajuste o caminho conforme necessário
+import { routes } from './app/app.routes'; // Certifique-se de importar o arquivo de rotas corretamente
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes)) // Configurando as rotas
+  ]
+}).catch(err => console.error(err));
